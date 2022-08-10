e6bf03da2c96fd7b0917b48475185d5ad570c06e
Nathan Oliveira <nateoliveira.dev@gmail.com>
2022-08-10 12:05:37 -0300

    requisitos 1, 4, 5 e 7 completos

---
+++ script.js
@@ -0,0 +1,24 @@
+const getTextFromInput = document.getElementById('text-input');
+const memeText = document.getElementById('meme-text');
+const imgMemeContainer = document.getElementById('meme-image-container');
+const fireBtn = document.getElementById('fire');
+const waterBtn = document.getElementById('water');
+const earthBtn = document.getElementById('earth');
+const presetImg = document.getElementsByTagName('img');
+const memeImg = document.getElementById('meme-image');
+
+for (let i = 1; i <= presetImg.length - 1; i += 1) {
+    presetImg[i].addEventListener('click', presetBackground);
+}
+
+getTextFromInput.addEventListener('input', textUpdate);
+
+function textUpdate() {
+    memeText.innerHTML = getTextFromInput.value;
+}
+
+function presetBackground(event) {
+    memeImg.style.width = '100%';
+    memeImg.style.height = "100%";
+    memeImg.src = event.target.src;
+}
