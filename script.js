6c5057b783fb8cd6d8c54677239e9191f704eccd
Nathan Oliveira <nateoliveira.dev@gmail.com>
2022-08-10 12:23:33 -0300

    fazendo os botoes e alteracao da borda

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ for (let i = 1; i <= presetImg.length - 1; i += 1) {
     presetImg[i].addEventListener('click', presetBackground);
 }
 
+fireBtn.addEventListener('click', borderChange);
+waterBtn.addEventListener('click', borderChange);
+earthBtn.addEventListener('click', borderChange);
 getTextFromInput.addEventListener('input', textUpdate);
 
 function textUpdate() {
@@ -19,6 +22,18 @@ function textUpdate() {
 
 function imgChange(event) {
     memeImg.src = URL.createObjectURL(event.target.files[0]);
+}
+
+function borderChange(event) {
+    if (event.target.id === 'fire') {
+        imgMemeContainer.style.border = '3px dashed rgb(255, 0, 0)';
+    }
+    if (event.target.id === 'water') {
+        imgMemeContainer.style.border = '5px double rgb(0, 0, 255)';
+    }
+    if (event.target.id === 'earth') {
+        imgMemeContainer.style.border = '6px groove rgb(0, 128, 0)';
+    }
 }
 
 function presetBackground(event) {
